cdf18b2b6932c1286a4a2523cb99f16e42202638
Antônio Lucena <victor_alb98@hotmail.com>
2023-03-23 20:03:02 -0300

    Organize_dataset created

---
+++ organize_dataset.py
@@ -0,0 +1,15 @@
+import os
+
+folder_path = "<caminho>"
+
+for folder in os.listdir(folder_path):
+
+    file_name_folder = os.path.join(folder_path, folder)
+
+    for file_name in os.listdir(file_name_folder):
+        # if i == 0:
+        #    print(file_name)
+        if file_name.endswith("_gt_segmentation.jpg" ) or file_name.endswith("_gt_ocr.txt"): # verifica se o nome do arquivo termina com "_gt_segmentation"
+            file_path = os.path.join(file_name_folder, file_name) # obtém o caminho completo do arquivo
+            os.remove(file_path) # exclui o arquivo
+
